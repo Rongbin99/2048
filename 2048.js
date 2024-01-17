75fd4365140ebad5ae5c4cab267164dc255c73a4
Rongbin Gu <rongbingu999@gmail.com>
2024-01-17 01:05:48 -0500

    tile movement complete :D

--- 2048.js
+++ 2048.js
@@ -10,7 +10,7 @@ window.onload = function() {
 //initializes the board
 function startGame() {
     board = [
-        [0, 2, 4, 0],
+        [2, 2, 4, 0],
         [0, 8, 64, 0],
         [0, 0, 32, 0],
         [16, 0, 0, 0]
@@ -47,7 +47,7 @@ function updateTile(tile, number) {
 
 /*keyboard inputs for moving tiles around*/
 
-document.addEventListener("keyup", (a) => {
+document.addEventListener("keydown", (a) => {
     if ((a.code == "ArrowLeft") || (a.code == "KeyA")) {
         moveLeft();
     }
@@ -68,23 +68,75 @@ function moveLeft() {
         let row = board[r];
         row = shift(row);
         board[r] = row;
+        //updates value at the tile
+        for (let c = 0; c < columns; c++) {
+            let tile = document.getElementById(r.toString() + "-" + c.toString());
+            let number = board[r][c];
+            updateTile(tile, number);
+        }
     }
 }
 
 function moveRight() {
-    
+    for (let r = 0; r < rows; r++) {
+        let row = board[r];
+        row = shift(row.reverse());
+        board[r] = row.reverse();
+        //updates value at the tile
+        for (let c = 0; c < columns; c++) {
+            let tile = document.getElementById(r.toString() + "-" + c.toString());
+            let number = board[r][c];
+            updateTile(tile, number);
+        }
+    }
 }
 
 function moveUp() {
-    
+    for (let c = 0; c < columns; c++) {
+        let row = [board[0][c], board[1][c], board[2][c], board[3][c]];
+        row = shift(row);
+        //updates value at the tile
+        for (let r = 0; r < rows; r++) {
+            board[r][c] = row[r]; //assigning array to be a column
+            let tile = document.getElementById(r.toString() + "-" + c.toString());
+            let number = board[r][c];
+            updateTile(tile, number);
+        }
+    }
 }
 
 function moveDown() {
-    
+    for (let c = 0; c < columns; c++) {
+        let row = [board[0][c], board[1][c], board[2][c], board[3][c]];
+        row = shift(row.reverse());
+        row = row.reverse(); //reverse back
+        //updates value at the tile
+        for (let r = 0; r < rows; r++) {
+            board[r][c] = row[r]; //assigning array to be a column
+            let tile = document.getElementById(r.toString() + "-" + c.toString());
+            let number = board[r][c];
+            updateTile(tile, number);
+        }
+    }
 }
 
-function shift() {
+function shift(row) {
+    row = row.filter(number => number != 0); //removes 0 from array
+    //shifts values over
+    for (let i = 0; i < row.length-1; i++) {
+        if (row[i] == row[i+1]) {//if adjacent values are the same
+            row[i] *= 2; //one gets doubled
+            row[i+1] = 0; //other turns into 0, or empty
+            score += row[i]; //increase score
+        }
+    }
+    row = row.filter(number => number != 0); //removes 0 from array again
 
+    //re-add 0's
+    while (row.length < columns) {
+        row.push(0);
+    }
+    return row;
 }
 
 //updates the score
